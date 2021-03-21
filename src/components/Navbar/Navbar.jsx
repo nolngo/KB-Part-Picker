4730bd44111c983e8a23e3b2212a83de7a02dc7d
Nolan Ngo <nolanngo@protonmail.com>
2021-03-21 17:33:45 -0400

    added cart in navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import {AppBar, Toolbar, IconButton, MenuItem, Menu, Typography} from '@material-ui/core';
+import { AppBar, Toolbar, IconButton, MenuItem, Menu, Typography, Badge } from '@material-ui/core';
+import { BuildRounded } from '@material-ui/icons'
 
 import useStyles from './styles';
 
@@ -10,9 +11,17 @@ const Navbar = () => {
       <AppBar position = "fixed" className = {classes.AppBar} color = "inherit">
         <Toolbar>
           <Typography variant="h6" className = {classes.title}>
-            <img src = './keyboard.svg' alt = "Which Switch" height = "25px" className = {classes.image} />
-            Which Switch?
+            <img src = './keyboard.svg' alt = "KBPARTPICKER" height = "25px" className = {classes.image} />
+            KBPARTPICKER
           </Typography>
+          <div className={classes.grow} />
+          <div className={classes.button}>
+            <IconButton aria-label="View Build Parts" color="inherit">
+              <Badge badgeContent={3} color="secondary">
+                <BuildRounded />
+              </Badge>
+            </IconButton>
+          </div>
         </Toolbar>
       </AppBar>
 
