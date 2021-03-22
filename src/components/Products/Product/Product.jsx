56f18c23763404f6fe7ad082014b808af21b99d0
Nolan Ngo <nolanngo@protonmail.com>
2021-03-21 20:45:59 -0400

    made functional add buttons

--- src/components/Products/Product/Product.jsx
+++ src/components/Products/Product/Product.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import { Card, CardMedia, CardContent, CardActions, Typography, IconButton } from '@material-ui/core';
-import { addShoppingCart } from '@material-ui/icons';
+import { AddCircleOutline } from '@material-ui/icons';
 
 import useStyles from './styles';
 
 const Product = ({ product }) => {
   const classes = useStyles();
+
+  function handleAdd (id) {
+    console.log(id)
+  }
     return (
         <Card className={classes.root}>
           <CardMedia className={classes.media} image={product.image} title={product.name} />
@@ -19,6 +23,11 @@ const Product = ({ product }) => {
                 {product.type}
               </Typography>
           </CardContent>
+          <CardActions disableSpacing className={classes.cardActions}>
+            <IconButton aria-label="Add to Build" onClick={(e) => handleAdd(product.id)}>
+              <AddCircleOutline />
+            </IconButton>
+          </CardActions>
         </Card>
     );
 }
